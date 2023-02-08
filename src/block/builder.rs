80ca3527e6225edb7033246d2efd22274fafae20
G1g4lo <3163083993@qq.com>
2023-02-09 00:06:17 +0800

    new

--- src/block/builder.rs
+++ src/block/builder.rs
@@ -4,23 +4,34 @@
 use super::Block;
 
 /// Builds a block.
-pub struct BlockBuilder {}
+pub struct BlockBuilder {
+    current_size : usize,
+    target_size : usize,
+    block : Box<Option<Block>>,
+}
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        return BlockBuilder { 
+            current_size: 0, 
+            target_size: block_size, 
+            block: Box::new(None) 
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
-        unimplemented!()
+        if self.current_size + key.len() + value.len() + 4 > self.target_size {
+            return false;
+        }
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.current_size == 0
     }
 
     /// Finalize the block.
