b2a7e03fa886e724f92f068eec86c50655b632d3
G1g4lo <3163083993@qq.com>
2023-02-09 21:46:36 +0800

    block

--- src/block.rs
+++ src/block.rs
@@ -5,9 +5,10 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Bytes, BufMut, Buf};
 pub use iterator::BlockIterator;
 
+pub const SIZEOF_U16: usize = std::mem::size_of::<u16>();
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted
 /// key-value pairs.
 pub struct Block {
@@ -17,11 +18,28 @@ pub struct Block {
 
 impl Block {
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut buf = self.data.clone();
+        for offset in &self.offsets {
+            buf.put_u16(*offset);
+        }
+        buf.put_u16(self.offsets.len() as u16);
+        buf.into()
     }
 
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let offsets_end = data.len() - SIZEOF_U16;
+        let num_entry = (&data[data.len() - SIZEOF_U16..]).get_u16();
+        let offsets_begin= data.len() - ((num_entry + 1) as usize) * SIZEOF_U16;
+
+        let decode_data = data[..offsets_begin].to_vec();
+        let decode_offsets = data[offsets_begin..offsets_end]
+            .chunks(SIZEOF_U16)
+            .map(|mut x| x.get_u16())
+            .collect();
+        Self {
+            data: decode_data,
+            offsets: decode_offsets,
+        }
     }
 }
 
--- src/block/builder.rs
+++ src/block/builder.rs
@@ -1,59 +1,65 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use super::Block;
+use bytes::BufMut;
+
+use super::{Block, SIZEOF_U16};
 
 /// Builds a block.
 pub struct BlockBuilder {
-    current_size: usize,
-    target_size: usize,
-    last_offset: u16,
-    num_of_elements: u16,
-    block: Box<Block>,
+    /// Offsets of each key-value entries.
+    offsets: Vec<u16>,
+    /// All key-value pairs in the block.
+    data: Vec<u8>,
+    /// The expected block size.
+    block_size: usize,
 }
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        return BlockBuilder { 
-            current_size: 0, 
-            target_size: block_size, 
-            last_offset: 0,
-            num_of_elements: 0,
-            block: Box::new(Block {data: Vec::new(), offsets: Vec::new()}) 
+        Self { 
+            offsets: Vec::new(), 
+            data: Vec::new(),
+            block_size,
         }
+    }
+
+    fn estimated_size(&self) -> usize {
+        self.offsets.len() * SIZEOF_U16 + self.data.len() + SIZEOF_U16
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
         // check size
-        if self.current_size + key.len() + value.len() + 7 > self.target_size { // 4 + 3
+        if self.estimated_size() + key.len() + value.len() + 3 * SIZEOF_U16 > self.block_size 
+            && !self.is_empty() { // 4 + 3
             return false;
         }
-        // append new entry in data vec
-        let key_len_b= (key.len() as u16).to_be_bytes();
-        let value_len_b = (value.len() as u16).to_be_bytes();
-        self.block.data.append(&mut key_len_b.to_vec());
-        self.block.data.append(&mut key.to_vec());
-        self.block.data.append(&mut value_len_b.to_vec());
-        self.block.data.append(&mut value.to_vec());
         // append offset in offset vec
-        self.block.offsets.push(self.last_offset);
-        // update builder
-        self.current_size += key.len() + value.len() + 6; // 4 + 2
-        self.last_offset = self.block.data.len() as u16; 
-        self.num_of_elements += 1;
+        self.offsets.push(self.data.len() as u16);
+        // append new entry in data vec
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key);
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
         true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        self.current_size == 0
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        *self.block
+        if self.is_empty() {
+            panic!("block should not be empty");
+        }
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
